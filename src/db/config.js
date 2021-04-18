828982ee0de3579054e9db17d16eee5c70dd4e0e
karlo <karlomr@gmail.com>
2021-04-18 17:45:19 -0300

    change init

--- src/db/config.js
+++ src/db/config.js
@@ -1,9 +1,16 @@
-const sqlite3 = require('sqlite3');
-const { open } = require('sqlite')
+//https://www.npmjs.com/package/@sap/hana-client
+//https://developers.sap.com/tutorials/hana-clients-node.html
+'use strict';
+const Database = require('@sap/hana-client');
 
-module.exports = () => 
-    open({
-        filename: "./database.sqlite",
-        driver: sqlite3.Database,
-    })
+//https://help.sap.com/viewer/f1b440ded6144a54ada97ff95dac7adf/2.7/en-US/4fe9978ebac44f35b9369ef5a4a26f4c.html
+const connOptions = {
+    //https://blogs.sap.com/2019/02/04/secure-user-store-for-the-sap-hana-service-by-the-sap-hana-academy/
+    //hdbuserstore
+    serverNode: '@USER1',  
+    sslValidateCertificate: 'false',
+    // HANA Cloud Platfomr only
+    //currentSchema: 'ROCKETSEAT',
+}
 
+module.exports = { Database:Database, connOptions:connOptions}
