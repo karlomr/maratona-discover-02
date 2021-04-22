fe743fe1c4c11d6473c2470520269645131a07c9
karlo <karlomr@gmail.com>
2021-04-22 09:20:12 -0300

    alter model Profile.js

--- src/model/Profile.js
+++ src/model/Profile.js
@@ -1,45 +1,53 @@
-const Database = require('../db/config')
-const { db, connOptions, open } = require("../db/config1")
+const Database = require("../db/config");
+const { db, connOptions } = require("../db/config1");
+
+function getData() {
+  return Promise((resolve, reject) => {
+    db.connect(connOptions, function (err) {
+      if (err) reject(err);
+      const statement = db.prepare(
+        `SELECT * FROM "ROCKETSEAT"."PROFILE" WHERE 1 =?`
+      );
+
+      statement.execQuery([1], function (err, rs) {
+        if (err) reject(err);
+
+        while (rs.next()) {
+          row.push(rs.getValues());
+          console.log("Rows: ", row);
+        }
+        //statement.drop();
+        resolve(row);
+      });
+    });
+
+    db.disconnect(function (err) {
+      if (err) {
+        reject(err);
+      }
+    });
+  });
+}
 
 module.exports = {
-    async get(){
-        db.connect(connOptions, function(err) {
-            if (err) throw err
-            const statement = db.prepare(`SELECT * FROM "ROCKETSEAT"."PROFILE" WHERE 1 =?`)  
-            statement.execQuery([1], function(err, rs) {
-              if (err) throw err
-              
-              while (rs.next()) {
-                rows.push(rs.getValues())
-                console.log("Rows: ", rows)
-              }
-              statement.drop();
-            })
-            
-            db.disconnect(function(err) {
-              if (err) {
-                  return console.error(err)
-              }   
-            })              
-        })    
-        //await db.close()
-        var data = {}
-        return {
-          name: data.name,
-          avatar: data.avatar,
-          "monthly-budget": data.monthly_budget,
-          "days-per-week": data.days_per_week,
-          "hours-per-day": data.hours_per_day,
-          "vacation-per-year": data.vacation_per_year,
-          "value-hour": data.value_hour
-        }
-    },
+  async get() {
+    const data = await getData();
+    return {
+      name: data.name,
+      avatar: data.avatar,
+      "monthly-budget": data.monthly_budget,
+      "days-per-week": data.days_per_week,
+      "hours-per-day": data.hours_per_day,
+      "vacation-per-year": data.vacation_per_year,
+      "value-hour": data.value_hour,
+    };
+  },
 
-    async update(newData){
-      const db = await Database()
+  async update(newData) {
+    const db = await Database();
 
-      db.run(
-        `UPDATE profile SET
+    db.run(
+      `UPDATE profile SET
            name = "${newData.name}" 
           ,avatar = "${newData.avatar}"
           ,monthly_budget = ${newData["monthly-budget"]}
@@ -47,8 +55,8 @@ module.exports = {
           ,hours_per_day = ${newData["hours-per-day"]}
           ,vacation_per_year = ${newData["vacation-per-year"]}
           ,value_hour = ${newData["value-hour"]}`
-      )
+    );
 
-      await db.close()      
-    }
-}
+    await db.close();
+  },
+};
